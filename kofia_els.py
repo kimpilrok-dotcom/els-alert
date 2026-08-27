ea5b7b1c7115f84ccda223eb57649dc790040335
kimpilrok-dotcom <kimpilrok@gmail.com>
2026-08-27 22:25:41 +0900

    Update kofia_els.py

--- kofia_els.py
+++ kofia_els.py
@@ -9,6 +9,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from webdriver_manager.core.os_manager import ChromeType
 
 def automate_download():
     DOWNLOAD_DIR = os.path.abspath(os.path.join(os.getcwd(), "downloads"))
